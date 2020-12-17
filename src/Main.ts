72f7a54e6ded3c5a7c8606a1ed68bbead3fb0731
Thomas Micol <thomasmicol@gmail.com>
2020-12-18 00:24:04 +1300

    added test method

--- src/Main.ts
+++ src/Main.ts
@@ -1,9 +1,23 @@
 import * as Konva from 'konva'
-import IRenderEngine from './'
+import BaseRenderEngine from './Classes/BaseRenderEngine';
+import IRenderEngine from './Interfaces/IRenderEngine'
 
-class App{
-    constructor(renderer: IRenderEngine)
-    {
+class App {
+    protected _render: IRenderEngine;
 
+    constructor(renderer: IRenderEngine) {
+        this._render = renderer;
     }
+
+    Prod() {
+        console.log("oh god", this._render);
+    }
+}
+
+const renderer = new BaseRenderEngine();
+
+const app = new App(renderer);
+
+function ClickEvent() {
+    app.Prod();
 }
